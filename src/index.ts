4dd6f4905f3cbeacbd5c99705fa38d7c292bc15c
Ali ABBAS <ali.ibra.abbas@gmail.com>
2025-05-23 16:29:34 +0200

    feat(server): register /api/signup, /api/login and protected /api/me routes

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,25 @@
-import bcrypt from 'bcryptjs'
-import cors from 'cors'
-import { PrismaClient } from '@prisma/client'
-import express from 'express'
+import cors from 'cors';
+import express from 'express';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import { signup, login } from './controllers/auth.controller';
 
-const prisma = new PrismaClient()
-const app = express()
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
 
 app.use(cors({
-  origin: ['http://localhost:8081', 'exp://10.2.164.186:8081'],
-  credentials: true
-}))
+  origin: 'http://localhost:8081',
+  credentials: true,
+}));
+app.use(express.json());
 
-app.use(express.json())
+// Routes d'authentification
+app.post('/api/signup', signup);
+app.post('/api/login', login);
 
-app.listen(3000, () =>
-  console.log('REST API server ready at: http://localhost:3000'),
-)
-
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
-})
-
-app.post('/createUser', async (req, res) => {
-  const { name, email, password } = req.body
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword
-    },
-  })
-  console.log(user)
-  res.json({name: user.name, email: user.email})
-})
-// async function main() {
-//   // ... your Prisma Client queries will go here
-//   const newUser = await prisma.user.create({
-//     data: {
-//       name: 'Alice',
-//       email: 'alice@prisma.io',
-//       hasGarden: true,
-//     },
-//   })
-//   console.log('Created new user: ', newUser)
-
-//   const allUsers = await prisma.user.findMany({
-//     select: { hasGarden: true },
-//   })
-//   console.log('All users: ')
-//   console.dir(allUsers, { depth: null })
-// }
-
-// main()
-//   .catch((e) => console.error(e))
-//   .finally(async () => await prisma.$disconnect())
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`REST API server ready at: http://localhost:${PORT}`)
+);
