e9b6a6c7c28c9a307086f9438b80a1b4498d9a87
deez-blip <gaspard.castagnet92@gmail.com>
2025-05-23 14:54:08 +0200

    update api: hashed password + allowed origin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs'
 import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
@@ -6,7 +7,7 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:8081',
+  origin: ['http://localhost:8081', 'exp://10.2.164.186:8081'],
   credentials: true
 }))
 
@@ -23,11 +24,12 @@ app.get('/users', async (req, res) => {
 
 app.post('/createUser', async (req, res) => {
   const { name, email, password } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
   const user = await prisma.user.create({
     data: {
       name,
       email,
-      password
+      password: hashedPassword
     },
   })
   res.json(user)
