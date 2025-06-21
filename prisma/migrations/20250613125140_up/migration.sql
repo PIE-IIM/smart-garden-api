1eca7115ccc7b129d89542bef0c1d264e2bb8b2a
JaxV9 <layanj.pro@gmail.com>
2025-06-21 11:04:24 +0200

    fix: change vegetable schema

--- prisma/migrations/20250613125140_up/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `plantation` to the `Vegetable` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Vegetable" ADD COLUMN     "plantation" TEXT NOT NULL;
