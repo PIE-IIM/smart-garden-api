27eda253401a6e91a1f01b9beb40a41edea0c67b
JaxV9 <layanj.pro@gmail.com>
2025-06-26 21:56:28 +0200

    fix schema

--- src/controllers/garden.controller.ts
+++ src/controllers/garden.controller.ts
@@ -1,31 +1,27 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { DefaultArgs } from "@prisma/client/runtime/library";
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { Utils } from "../utils";
 import { Vegetable, vegetables } from "../../constants/data";
 
 export class GardenController {
-  constructor(
-    private prisma: PrismaClient,
-    private utils: Utils
-  ) {}
+  constructor(private prisma: PrismaClient, private utils: Utils) {}
 
   //use to generate vegetables in database
   async putVegetables(req: Request<{}, {}, Vegetable>, res: Response) {
-    vegetables.map(async element => {
-            await this.prisma.vegetable.create({
-                data: {
-                    name: element.name,
-                    description: element.description,
-                    specifications: element.caracteristiques,
-                    sowing: element.semis,
-                    plantation: element.plantation,
-                    harvest: element.recolte,
-                    affinity: element.affinites,
-                    bad_neighbors: element.mauvais_voisins
-                }
-            })
-        })
+    vegetables.map(async (element) => {
+      await this.prisma.vegetable.create({
+        data: {
+          name: element.name,
+          description: element.description,
+          specifications: element.specifications,
+          sowing: element.sowing,
+          plantation: element.plantation,
+          harvest: element.harvest,
+          affinity: element.affinity,
+          bad_neighbors: element.bad_neighbors,
+        },
+      });
+    });
     res.status(201).json();
     return;
   }
