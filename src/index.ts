418d7a7ca314f3ec33eea7ab8056905d2760a829
Ali ABBAS <ali.ibra.abbas@gmail.com>
2025-06-27 20:09:15 +0200

    feat(routes):  add new routes

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import { UserController } from './controllers/user.controller';
+import { PotagerController } from './controllers/potager.controller';
 import { Utils } from './utils';
 import { GardenController } from './controllers/garden.controller';
+import { authenticateToken } from './middleware/auth.middleware'; 
 
 dotenv.config();
 const app = express();
@@ -12,6 +14,8 @@ const prisma = new PrismaClient();
 const utils = new Utils();
 const userController = new UserController(prisma, utils);
 const gardenController = new GardenController(prisma, utils);
+const potagerController = new PotagerController(prisma, utils);
+
 
 app.use(cors({
   origin: 'http://localhost:8081',
@@ -31,6 +35,21 @@ app.post('/api/genvegetables', async (req, res) => {
   await gardenController.putVegetables(req, res);
 })
 
+app.get('/api/vegetables', (req, res) =>
+  gardenController.getAll(req, res)
+);
+
+app.get('/api/user/vegetables', authenticateToken, (req, res) =>
+  potagerController.list(req, res)
+);
+
+app.post('/api/user/vegetable', authenticateToken, (req, res) =>
+  potagerController.add(req, res)
+);
+
+app.delete('/api/user/vegetable/:id', authenticateToken, (req, res) =>
+  potagerController.remove(req, res)
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
