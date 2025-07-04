f6085a8d1f733009a4afe50a740ae2ff540d7c4b
JaxV9 <layanj.pro@gmail.com>
2025-07-04 12:16:21 +0200

    feat: vegetables image

---
+++ prisma/migrations/20250704094909_init/migration.sql
@@ -0,0 +1,78 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expireAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vegetable" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "specifications" TEXT[],
+    "sowing" TEXT[],
+    "plantation" TEXT[],
+    "harvest" TEXT[],
+    "affinity" TEXT[],
+    "bad_neighbors" TEXT[],
+
+    CONSTRAINT "Vegetable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VagetableImage" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "vegetableId" TEXT NOT NULL,
+
+    CONSTRAINT "VagetableImage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Potager" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "vegetableId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Potager_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Token_userId_key" ON "Token"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VagetableImage_vegetableId_key" ON "VagetableImage"("vegetableId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Potager_userId_vegetableId_key" ON "Potager"("userId", "vegetableId");
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VagetableImage" ADD CONSTRAINT "VagetableImage_vegetableId_fkey" FOREIGN KEY ("vegetableId") REFERENCES "Vegetable"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Potager" ADD CONSTRAINT "Potager_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Potager" ADD CONSTRAINT "Potager_vegetableId_fkey" FOREIGN KEY ("vegetableId") REFERENCES "Vegetable"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/controllers/garden.controller.ts
+++ src/controllers/garden.controller.ts
@@ -1,26 +1,42 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { Utils } from "../utils";
-import { Vegetable, vegetables } from "../../constants/data";
+import { Vegetable, vegetables, vegetableImages } from "../../constants/data";
 
 export class GardenController {
   constructor(private prisma: PrismaClient, private utils: Utils) {}
 
   //use to generate vegetables in database
   async putVegetables(req: Request<{}, {}, Vegetable>, res: Response) {
-    vegetables.map(async (element) => {
+    vegetables.map(async (vegetable) => {
       await this.prisma.vegetable.create({
         data: {
-          name: element.name,
-          description: element.description,
-          specifications: element.specifications,
-          sowing: element.sowing,
-          plantation: element.plantation,
-          harvest: element.harvest,
-          affinity: element.affinity,
-          bad_neighbors: element.bad_neighbors,
+          name: vegetable.name,
+          description: vegetable.description,
+          specifications: vegetable.specifications,
+          sowing: vegetable.sowing,
+          plantation: vegetable.plantation,
+          harvest: vegetable.harvest,
+          affinity: vegetable.affinity,
+          bad_neighbors: vegetable.bad_neighbors,
         },
       });
+
+      const vegetableImage = vegetableImages.find(
+        (element) => element.name === vegetable.name
+      );
+      const currentVegetable = await this.prisma.vegetable.findFirst({
+        where: { name: vegetable.name },
+      });
+
+      if (vegetableImage && currentVegetable) {
+        await this.prisma.vegetableImage.create({
+          data: {
+            url: vegetableImage.url,
+            vegetableId: currentVegetable.id,
+          },
+        });
+      }
     });
     res.status(201).json();
     return;
@@ -28,7 +44,9 @@ export class GardenController {
 
   // GET /api/vegetables (public)
   async getAll(_req: Request, res: Response) {
-    const list = await this.prisma.vegetable.findMany({ orderBy: { name: 'asc' } });
+    const list = await this.prisma.vegetable.findMany({
+      orderBy: { name: "asc" },
+    });
     res.status(200).json(list);
   }
 }
