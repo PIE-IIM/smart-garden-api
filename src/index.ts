31d2a52911adfe093ef9c3523b2a2d290b340ce6
JaxV9 <layanj.pro@gmail.com>
2025-12-02 20:55:18 +0100

    remove useless routes

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,7 @@ app.post("/api/login", async (req, res) => {
   await userController.login(req, res);
 });
 
-app.post("/api/genvegetables", async (req, res) => {
-  await gardenController.putVegetables(req, res);
-});
-
-app.get("/api/vegetables", (req, res) => gardenController.getAll(req, res));
+// app.get("/api/vegetables", (req, res) => gardenController.getAll(req, res));
 
 app.get("/api/user", authenticateToken, async (req, res) => {
   await userController.getUser(req, res);
